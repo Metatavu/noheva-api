0da27a7ddda50b9d5e002d50e4b4dd2bb82234bd
Antti Leppä <antti.leppa@metatavu.fi>
2021-02-17 15:59:46 +0200

    Added dedicated exception to copy operation failures

---
+++ src/main/kotlin/fi/metatavu/muisti/utils/CopyException.kt
@@ -0,0 +1,10 @@
+package fi.metatavu.muisti.utils
+
+/**
+ * Exception that is thrown when error occurs while copying
+ *
+ * @param message message
+ * @author Antti Leppä
+ */
+class CopyException(message: String): Exception(message) {
+}
