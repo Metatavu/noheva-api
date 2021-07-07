254a322bffc1bdcf06839ab1a551a0648eb43635
Antti Leppä <antti.leppa@metatavu.fi>
2021-07-07 16:00:02 +0300

    Added endpoint for inspecting memory

--- src/main/kotlin/fi/metatavu/muisti/api/SystemApiImpl.kt
+++ src/main/kotlin/fi/metatavu/muisti/api/SystemApiImpl.kt
@@ -1,8 +1,10 @@
 package fi.metatavu.muisti.api
 
 import fi.metatavu.muisti.api.spec.SystemApi
+import org.apache.commons.io.FileUtils
 import javax.enterprise.context.RequestScoped
 import javax.transaction.Transactional
+import javax.ws.rs.Path
 import javax.ws.rs.core.Response
 
 
@@ -17,6 +19,18 @@ open class SystemApiImpl(): SystemApi {
 
     override fun ping(): Response? {
         return Response.ok("pong").build()
+    }
+
+    @Path("/memory")
+    fun memory(): Response? {
+        val runtime = Runtime.getRuntime();
+
+        val result = mutableMapOf<String, String>()
+        result.put("freeMemory", FileUtils.byteCountToDisplaySize(runtime.freeMemory()))
+        result.put("availableProcessors", runtime.availableProcessors().toString())
+        result.put("maxMemory", FileUtils.byteCountToDisplaySize(runtime.maxMemory()))
+
+        return Response.ok(result).build()
     }
 
 }
