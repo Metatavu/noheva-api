0b549a3cf781a239c1e83f1a1bee3a1563e5204f
Antti Leppä <antti.leppa@metatavu.fi>
2021-07-07 17:37:39 +0300

    Added endpoint for inspecting memory

--- src/main/kotlin/fi/metatavu/muisti/api/SystemApiImpl.kt
+++ src/main/kotlin/fi/metatavu/muisti/api/SystemApiImpl.kt
@@ -4,7 +4,9 @@ import fi.metatavu.muisti.api.spec.SystemApi
 import org.apache.commons.io.FileUtils
 import javax.enterprise.context.RequestScoped
 import javax.transaction.Transactional
+import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.Produces
 import javax.ws.rs.core.Response
 
 
@@ -21,7 +23,9 @@ open class SystemApiImpl(): SystemApi {
         return Response.ok("pong").build()
     }
 
+    @GET
     @Path("/memory")
+    @Produces("application/json")
     fun memory(): Response? {
         val runtime = Runtime.getRuntime();
 
