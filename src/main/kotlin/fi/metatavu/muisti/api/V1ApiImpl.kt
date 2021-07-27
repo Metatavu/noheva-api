e0e52f2d85c29add7c4a69ce55d94081f131a63e
Antti Leppä <antti.leppa@metatavu.fi>
2021-07-27 10:48:05 +0300

    Fixed typo for loggedUserId

--- src/main/kotlin/fi/metatavu/muisti/api/V1ApiImpl.kt
+++ src/main/kotlin/fi/metatavu/muisti/api/V1ApiImpl.kt
@@ -294,7 +294,7 @@ class V1ApiImpl: V1Api, AbstractApi() {
         exhibitionId ?: return createNotFound(EXHIBITION_NOT_FOUND)
         visitorId ?: return createNotFound(VISITOR_NOT_FOUND)
 
-        val userId = loggerUserId ?: return createUnauthorized(UNAUTHORIZED)
+        val userId = loggedUserId ?: return createUnauthorized(UNAUTHORIZED)
         exhibitionController.findExhibitionById(exhibitionId) ?: return createNotFound("Exhibition $exhibitionId not found")
         val visitor = visitorController.findVisitorById(visitorId) ?: return createNotFound("Visitor $visitorId not found")
 
